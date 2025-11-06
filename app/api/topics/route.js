aa9fd3e04207703e72b2c71b22a51a5ae65df6df
Rojina Rai <roji71418@gmail.com>
2025-11-06 12:12:49 +0545

    modified

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -1,12 +1,13 @@
+// app/api/topics/route.js
 import { NextResponse } from "next/server";
-import Topic from "../../libs/models/topic";
-import connectMongoDB from "../../libs/mongodb";
+import Topic from "@/lib/models/topic";           // FIXED
+import connectMongoDB from "@/lib/mongodb";       // FIXED
 
 export async function POST(request) {
     const { title, description } = await request.json();
     await connectMongoDB();
     await Topic.create({ title, description });
-    return NextResponse.json({ message: "Topic Created" }, {status: 201 });
+    return NextResponse.json({ message: "Topic Created" }, { status: 201 });
 }
 
 export async function GET() {
