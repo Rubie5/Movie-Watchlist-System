7a366ac15a91bb96e8913ef303167032b67808b0
Rojina Rai <roji71418@gmail.com>
2025-11-06 22:00:36 +0545

    route.js is updated.

--- app/api/topics/[id]/route.js
+++ app/api/topics/[id]/route.js
@@ -3,9 +3,15 @@ import { NextResponse } from "next/server";
 import Topic from "@/libs/models/topic";
 
 export async function PUT(request,{params}){
-    const { id } = params;
+    const { id } = await params;
     const { newtitle: title, newdescription: description } = await request.json();
     await connectMongoDB();
     await Topic.findByIdAndUpdate(id, { title, description });
     return NextResponse.json({ message: "Topic updated" }, { status: 200 });
+}
+export async function GET(request,{params}){
+    const { id } = await params;
+    await connectMongoDB();
+    const topic =  await Topic.findById(id);
+    return NextResponse.json({ topic }, { status: 200 });
 }
